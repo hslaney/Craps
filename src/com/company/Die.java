ed42a86a1bd1d9e8a29d630f305c72fd83ee1a51
hslaney <haley.slaney@gmail.com>
2020-11-13 13:40:11 -0500

    Initial commit

---
+++ src/com/company/Die.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Die {
+
+    public void roll{
+        int x = 
+    }
+
+
+    public static void main (String[] args){
+        //TODO Auto-generated methods stub
+        Die die = new Die();
+        die.roll();
+        System.out.println(die.getNumDots());
+
+
+    }
+}
